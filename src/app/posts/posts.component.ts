3699ae31c477d67925c6f5070a8da1d99a517b78
lmmontejo85926 <lmmontejo85926@liceo.edu.ph>
2026-02-19 15:10:10 +0800

    posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,27 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
+  standalone: true,
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-
-  posts = [
-    {
-      title: 'Community Update',
-      description: 'Recent announcements and activities shared with members of the community.'
-    },
-    {
-      title: 'New Features Released',
-      description: 'We’ve added improvements to make the platform faster, cleaner, and easier to use.'
-    },
-    {
-      title: 'Weekly Highlights',
-      description: 'A recap of the most important updates and discussions from this week.'
-    }
-  ];
-
-  deletePost(index: number) {
-    this.posts.splice(index, 1);
-  }
-}
+export class PostsComponent {}
